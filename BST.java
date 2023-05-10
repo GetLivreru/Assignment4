dff96e5890199882bcb24404be08640da912e5e4
theadilet <110306539+GetLivreru@users.noreply.github.com>
2023-05-11 00:29:46 +0600

    Update BST.java
    add comments

--- BST.java
+++ BST.java
@@ -3,43 +3,43 @@ import java.util.Stack;
 
 public class BST<K extends Comparable<K>, V> implements Iterable<BST.Node<K,V>> {
 
-    private Node<K, V> root;
-    private int size;
+    private Node<K, V> root; // root node of the binary search tree
+    private int size; // number of nodes in the binary search tree
 
     public static class Node<K, V> {
-        private K key;
-        private V value;
-        private Node<K, V> left, right;
+        private K key; // key associated with the node
+        private V value; // value associated with the node
+        private Node<K, V> left, right; // left and right children of the node
 
-        public Node(K key, V value) {
+        public Node(K key, V value) {  // constructor
             this.key = key;
             this.value = value;
         }
     }
     
     
-public void put(K key, V value) {
-        root = put(root, key, value);
+public void put(K key, V value) { // insert a key-value pair into the binary search tree
+        root = put(root, key, value); // recursively insert the node
     }
     private Node<K, V> put(Node<K, V> node, K key, V value) {
         if (node == null) {
-            size++;
+            size++; // increment the size of the binary search tree
             return new Node<>(key, value);
         }
-        int cmp = key.compareTo(node.key);
-        if (cmp < 0) {
+        int cmp = key.compareTo(node.key); // compare the given key with the key of the current node
+        if (cmp < 0) { // if the given key is less than the key of the current node, insert the key-value pair into the left subtree
             node.left = put(node.left, key, value);
-        } else if (cmp > 0) {
+        } else if (cmp > 0) { // if the given key is greater than the key of the current node, insert the key-value pair into the right subtree
             node.right = put(node.right, key, value);
-        } else {
+        } else { // if the given key is equal to the key of the current node, update the value of the node with the given value
             node.value = value;
         }
         return node;
     }
     
-          public V get(K key) {
-        Node<K, V> node = get(root, key);
-        return node == null ? null : node.value;
+          public V get(K key) { // get the value associated with the given key
+        Node<K, V> node = get(root, key); // recursively get the node with the given key
+        return node == null ? null : node.value; // return the value of the node if it exists, otherwise return null
     }
     
     private Node<K, V> get(Node<K, V> node, K key) {
@@ -55,6 +55,31 @@ public void put(K key, V value) {
             return node;
         }
     }
-    public void delete(K key) {
-        root = delete(root, key);
+    public void delete(K key) {  // delete the node with the given key from the binary search tree
+        root = delete(root, key); // recursively delete the node
+    }
+    private Node<K, V> delete(Node<K, V> node, K key) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = delete(node.left, key);
+        } else if (cmp > 0) {
+            node.right = delete(node.right, key);
+        } else {
+            if (node.right == null) {
+                size--;
+                return node.left;
+            }
+            if (node.left == null) {
+                size--;
+                return node.right;
+            }
+            Node<K, V> temp = node;
+            node = min(temp.right);
+            node.right = deleteMin(temp.right);
+            node.left = temp.left;
+        }
+        return node;
     }
