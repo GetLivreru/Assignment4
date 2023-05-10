1a9781c089bd4b4efed5a4e8ae2e4063f62a11ce
theadilet <110306539+GetLivreru@users.noreply.github.com>
2023-05-10 20:38:28 +0600

    Update BST.java

--- BST.java
+++ BST.java
@@ -16,3 +16,28 @@ public class BST<K extends Comparable<K>, V> implements Iterable<BST.Node<K,V>> 
             this.value = value;
         }
     }
+    
+    
+public void put(K key, V value) {
+        root = put(root, key, value);
+    }
+    private Node<K, V> put(Node<K, V> node, K key, V value) {
+        if (node == null) {
+            size++;
+            return new Node<>(key, value);
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = put(node.left, key, value);
+        } else if (cmp > 0) {
+            node.right = put(node.right, key, value);
+        } else {
+            node.value = value;
+        }
+        return node;
+    }
+    
+          public V get(K key) {
+        Node<K, V> node = get(root, key);
+        return node == null ? null : node.value;
+    }
